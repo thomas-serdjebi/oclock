c42e286e3ab8a42bc2d7af273fa18633fd0a2344
Thomas <thomas.serdjebi@laplateforme.io>
2022-05-16 16:00:26 +0200

    Création de l'index avec les inputs et le style nécessaire à la page

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+    <meta charset="utf-8">
+    <title>Oclock</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Aclonica&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <section class="container">
+            <div id="clock"></div>
+            <input name="alarmTime" type="datetime-local">
+            <div class="controls">
+            <button class="button set-alarm">Set alarm</button>
+                <button class="button clear-alarm">Clear alarm</button>
+            </div>
+            
+
+        </section>
+    
+    </body>
+</html>
