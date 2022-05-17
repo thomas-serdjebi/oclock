6401a246397b64d43f1ff207a028229dba25fd1f
Thomas <thomas.serdjebi@laplateforme.io>
2022-05-17 15:11:42 +0200

    Construction de la base pour le minuteur

--- index.php
+++ index.php
@@ -12,12 +12,29 @@
 
     <body>
         <section class="container">
-            <div id="clock"></div>
-            <input type="datetime-local" name="alarm" id="alarm">
-            <div class="controls">
-                <button class="button set-alarm" id="set-alarm">Set alarm</button>
-                <button class="button clear-alarm" id="clear-alarm">Clear alarm</button>
+
+            <!-- HORLOGE + ALARM -->
+
+            <div>
+                <div id="clock"></div>
+                <input type="datetime-local" name="alarm" id="alarm">
+                <div class="controls">
+                    <button class="button set-alarm" id="set-alarm">Set alarm</button>
+                    <button class="button clear-alarm" id="clear-alarm">Clear alarm</button>
+                </div>
             </div>
+
+            <!-- MINUTEUR -->
+
+            <div id="countdown-timer">
+                <p id="hour-label" class="label">Hours</p><p id="min-label" class="label">Minutes</p><p id="sec-label" class="label">Seconds</p>
+                <input id="hour" type="number" max="99" min="0" value="0" class="time">
+
+            </div>
+
+
+
+
         </section>
     
     </body>
