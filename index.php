e8db9e3a19c5927a71b9cc298d7f788500ff88d9
Thomas <thomas.serdjebi@laplateforme.io>
2022-05-18 17:40:21 +0200

    Minuteur fonctionnel

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         <div id="countdown-timer">
 
             <p id="hour-label" class="label">Hours</p><p id="min-label" class="label">Minutes</p><p id="sec-label" class="label">Seconds</p>
-            <input id="hour" type="number" max="99" min="0" value="0" class="time"><p id="p1" class="semicolon">:</p><input id="minute" type="number" max="60" min="0" value="0" class="time"><p id="p2" class="semicolon">:</p><input id="second" type="number" max="60" min="0" value="0" class="time">
+            <input id="hour" type="number" max="99" min="0" value="0"class="time"><p id="p1" class="semicolon">:</p><input id="minute" type="number" max="60" min="0" value="0" class="time"><p id="p2" class="semicolon">:</p><input id="second" type="number" max="60" min="0" value="0" class="time">
             <button id="start" class="btn">Start</button>
             <button id="reset" class="btn">Reset</button>
 
